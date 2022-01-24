498ae4b2ff6de66ca3c0a7a2f1360c5b013d73c5
shyzik93 <shyzik93@mail.ru>
2022-01-25 00:06:42 +0300

    add settings for running server locally

--- django_knowledge/settings.py
+++ django_knowledge/settings.py
@@ -4,7 +4,49 @@ from dotenv import load_dotenv
 
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+ALLOWED_HOSTS = ['*']
+ROOT_URLCONF = 'django_knowledge.urls'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_knowledge',
+    'rest_framework',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+     {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'j^)*90mk@i&-3$9p)0-g^!+m3)fdx^08moesscx=8=8jqr%&g0')
+
+# KNOWLEDGE
 
 GITHUB_OWNER = os.getenv('GITHUB_OWNER', 'TVP-Support')
 GITHUB_REPO = os.getenv('GITHUB_REPO', 'knowledge')
