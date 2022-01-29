6eeb6acc3e661e61bb21c6f6511e3cd2a1b2d186
shyzik93 <shyzik93@mail.ru>
2022-01-29 14:22:16 +0300

    prepare for deploying on beget hosting

--- django_knowledge/settings.py
+++ django_knowledge/settings.py
@@ -1,12 +1,16 @@
 import os
 
-from dotenv import load_dotenv
+import environ
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-load_dotenv(os.path.join(BASE_DIR, 'django_knowledge/.env'))
+env = environ.Env(
+    DEBUG=(bool, True),
+    ALLOWED_HOSTS=(list, ['*']),
+)
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 ROOT_URLCONF = 'django_knowledge.urls'
 WSGI_APPLICATION = 'django_knowledge.wsgi.application'
 
@@ -48,28 +52,28 @@ TEMPLATES = [
     },
 ]
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'j^)*90mk@i&-3$9p)0-g^!+m3)fdx^08moesscx=8=8jqr%&g0')
+SECRET_KEY = env('SECRET_KEY')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'sqlite3.db'),
     }
 }
 
 # KNOWLEDGE
 
-GITHUB_OWNER = os.getenv('GITHUB_OWNER', 'TVP-Support')
-GITHUB_REPO = os.getenv('GITHUB_REPO', 'knowledge')
-GITHUB_DIRECTORY = os.getenv('GITHUB_DIRECTORY', 'db')
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN', 'your_token')
+GITHUB_OWNER = env('GITHUB_OWNER')
+GITHUB_REPO = env('GITHUB_REPO')
+GITHUB_DIRECTORY = env('GITHUB_DIRECTORY')
+GITHUB_TOKEN = env('GITHUB_TOKEN')
 
-FIRESTORE_CERTIFICATE = os.getenv('FIRESTORE_CERTIFICATE', 'knowledge.json')
+FIRESTORE_CERTIFICATE = env('FIRESTORE_CERTIFICATE')
 
-TYPESENSE_SERVER = os.getenv('TYPESENSE_SERVER', 'localhost')
-TYPESENSE_PORT = int(os.getenv('TYPESENSE_PORT', '8108'))
-TYPESENSE_PROTOCOL = os.getenv('TYPESENSE_PROTOCOL', 'http')
-TYPESENSE_API_KEY = os.getenv('TYPESENSE_API_KEY', 'your_any_key')
+TYPESENSE_SERVER = env('TYPESENSE_SERVER')
+TYPESENSE_PORT = int(env('TYPESENSE_PORT'))
+TYPESENSE_PROTOCOL = env('TYPESENSE_PROTOCOL')
+TYPESENSE_API_KEY = env('TYPESENSE_API_KEY')
 
-DEFAULT_DOWNLOADER = os.getenv('DEFAULT_DOWNLOADER', 'github_archive')
-DEFAULT_UPLOADER = os.getenv('DEFAULT_UPLOADER', 'typesense')
+DEFAULT_DOWNLOADER = env('DEFAULT_DOWNLOADER')
+DEFAULT_UPLOADER = env('DEFAULT_UPLOADER')
