cdd0d322ec52895c0b25732d224fcbd8e8cc63da
shyzik93 <shyzik93@mail.ru>
2022-01-30 03:14:36 +0300

    new api format

--- django_knowledge/views.py
+++ django_knowledge/views.py
@@ -10,9 +10,27 @@ from note.credentials import args_uploader
 
 @api_view(('GET',))
 @renderer_classes((JSONRenderer,))
-def note_search(request):
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+def note_search(request, query):
+    search_by = request.GET.get('search-by', 'all')
+    if search_by not in ('content', 'title', 'all'):
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': 'Invalid `search-by` parameter'})
+
+    file_name = query if search_by in ('title', 'all') else None
+    file_content = query if search_by in ('content', 'all') else None
+    operator = request.GET.get('operator', 'all')
+    limit = int(request.GET.get('limit', '10'))
+    offset = int(request.GET.get('offset', '0'))
+    fields = request.GET.get('fields', 'title')
+    fields = ('title', 'content') if fields == 'all' else (fields,)
     uploader = request.GET.get('source', settings.DEFAULT_UPLOADER)
-    results = search(uploader, args_uploader[uploader], file_name=title, file_content=content)
-    return Response(status=status.HTTP_200_OK, data={'success': True, 'data': results})
+    data = search(
+        uploader,
+        args_uploader[uploader],
+        operator=operator,
+        limit=limit,
+        offset=offset,
+        fields=fields,
+        file_name=file_name,
+        file_content=file_content,
+    )
+    return Response(status=status.HTTP_200_OK, data=data)
