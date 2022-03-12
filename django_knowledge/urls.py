3059a3414182322b1c0bdc38d8877e1d99c9117e
shyzik93 <shyzik93@mail.ru>
2022-03-12 14:28:39 +0300

    fix push

--- django_knowledge/urls.py
+++ django_knowledge/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from note.views import note_search
+from note.views import note_search, note_hook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/note/search/<str:query>/', note_search, name='api_note_search'),
+    path('api/v1/note/hook/', note_hook, name='api_note_hook'),
 ]
