8c8ed4973cd216c57d180cbd2d60f9151d3fdf9d
shyzik93 <shyzik93@mail.ru>
2022-03-13 10:10:13 +0300

    add DEBUG

--- django_knowledge/settings.py
+++ django_knowledge/settings.py
@@ -11,6 +11,7 @@ env = environ.Env(
 environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+DEBUG = env('DEBUG')
 ROOT_URLCONF = 'django_knowledge.urls'
 WSGI_APPLICATION = 'django_knowledge.wsgi.application'
 
