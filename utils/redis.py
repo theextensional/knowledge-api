933dd7621296f22b1b101681f10b447b10884a70
shyzik93 <shyzik93@mail.ru>
2022-05-03 02:08:45 +0300

    add passing password into redis

--- utils/redis.py
+++ utils/redis.py
@@ -3,4 +3,9 @@ from django.conf import settings
 
 
 def get_redis():
-    return redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
+    return redis.StrictRedis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        password=settings.REDIS_PASSWORD,
+        db=0,
+    )
